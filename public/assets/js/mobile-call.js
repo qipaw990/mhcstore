e8de2bae7d4e15dacdbda24f06ccc0d1ac4aae4c
qipaw990 <rizkifahrezi990@gmail.com>
2026-08-24 01:46:12 +0700

    feat: add OpenRelay TURN servers for guaranteed cross-network calling over 4G/5G cellular data & different Wi-Fi networks

--- public/assets/js/mobile-call.js
+++ public/assets/js/mobile-call.js
@@ -28,7 +28,22 @@
             { urls: 'stun:stun2.l.google.com:19302' },
             { urls: 'stun:stun3.l.google.com:19302' },
             { urls: 'stun:stun4.l.google.com:19302' },
-            { urls: 'stun:global.stun.twilio.com:3478' }
+            { urls: 'stun:global.stun.twilio.com:3478' },
+            {
+                urls: 'turn:openrelay.metered.ca:80',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+            },
+            {
+                urls: 'turn:openrelay.metered.ca:443',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+            },
+            {
+                urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+            }
         ],
         iceCandidatePoolSize: 10
     };
