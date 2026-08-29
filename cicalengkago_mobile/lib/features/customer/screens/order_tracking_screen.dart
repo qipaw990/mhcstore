d0159fc7ee1734286cf1e0646ad5df5d18049eb4
qipaw990 <rizkifahrezi990@gmail.com>
2026-08-30 03:45:29 +0700

    fix(customer-tracking): tambahkan import dart:math untuk kalkulasi math.max pada ETA

--- cicalengkago_mobile/lib/features/customer/screens/order_tracking_screen.dart
+++ cicalengkago_mobile/lib/features/customer/screens/order_tracking_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
