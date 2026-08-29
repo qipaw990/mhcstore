2f64495f059788ebe7bcd4b150964575a60aac32
qipaw990 <rizkifahrezi990@gmail.com>
2026-08-30 06:48:34 +0700

    fix(mobile): tambahkan import services.dart untuk Clipboard di customer_wallet_screen.dart

--- cicalengkago_mobile/lib/features/customer/screens/customer_wallet_screen.dart
+++ cicalengkago_mobile/lib/features/customer/screens/customer_wallet_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../../core/constants/api_constants.dart';
