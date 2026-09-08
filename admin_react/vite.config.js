51cc61583dbc3ed3737fc0dfa4ec4c978c074483
qipaw990 <rizkifahrezi990@gmail.com>
2026-09-08 23:24:22 +0700

    perf: Optimize code splitting chunks & eliminate 500kB warning

--- admin_react/vite.config.js
+++ admin_react/vite.config.js
@@ -5,4 +5,25 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('recharts')) {
+              return 'vendor-charts';
+            }
+            if (id.includes('lucide-react')) {
+              return 'vendor-icons';
+            }
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'vendor-react';
+            }
+            return 'vendor-libs';
+          }
+        }
+      }
+    }
+  }
 })
